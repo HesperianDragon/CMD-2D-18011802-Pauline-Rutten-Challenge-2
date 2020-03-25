0dd0145a047c8d258ba02befaf3a6a0935e8790b
HesperianDragon <42208828+HesperianDragon@users.noreply.github.com>
2020-03-25 20:33:57 +0100

    Working version with clock

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+// function myFunction(){
+//     // console.log('hi')
+//     var d = new Date();
+//     var monthNames = ["January", "February", "March", "April", "May", "June",
+//     "July", "August", "September", "October", "November", "December"];
+//     var dayNames =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//     // document.getElementById('year').innerHTML = d.getYear(); //Returns year
+//     document.getElementById('month').innerHTML = monthNames[d.getMonth()]; //Returns month
+//     document.getElementById('date').innerHTML = dayNames[d.getDay()]; //Returns the day of the week
+//     // document.getElementById('hour').innerHTML = d.getHours(); //Returns hours
+//     // document.getElementById('minutes').innerHTML = d.getMinutes(); //Returns minutes
+//     // document.getElementById('seconds').innerHTML = d.getMinutes(); //Returns seconds
+//     console.log('hi')
+    
+    
+// }
