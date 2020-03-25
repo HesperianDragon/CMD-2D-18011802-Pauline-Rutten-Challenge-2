38f2cc3d194d947cea40a494fe4145ed97d4e9a1
HesperianDragon <42208828+HesperianDragon@users.noreply.github.com>
2020-03-25 23:15:29 +0100

    Final version

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CMD-2D-18011802-Pauline-Rutten-Challenge-2
  Responsive Webdesign op Mars
+
+Voor dit ontwerp is er een analoge klok ontworpen, waarvan de wijzers om de seconde worden aangepast naar de actuele tijd. 
+
+De reiziger op Mars zal niet alleen nieuwsgierig zijn naar de actuele tijd, maar ook naar de datum. In het ontwerp wordt de dagnaam, maandnaam, dagnummer en jaartal weergeven. 
+
+Het ontwerp bestaat uit twee soorten kleurenpalleten. Eén voor de dagweergave en één voor de nachtweergave. 
+Voor de dagweergave wordt er gebruik gemaakt voor een lichtere kleurenpalette, terwijl voor de nachtversie gebruik wordt gemaakt van een donkere kleurenpallette. 
--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,33 @@
-// function myFunction(){
-//     // console.log('hi')
-//     var d = new Date();
-//     var monthNames = ["January", "February", "March", "April", "May", "June",
-//     "July", "August", "September", "October", "November", "December"];
-//     var dayNames =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-//     // document.getElementById('year').innerHTML = d.getYear(); //Returns year
-//     document.getElementById('month').innerHTML = monthNames[d.getMonth()]; //Returns month
-//     document.getElementById('date').innerHTML = dayNames[d.getDay()]; //Returns the day of the week
-//     // document.getElementById('hour').innerHTML = d.getHours(); //Returns hours
-//     // document.getElementById('minutes').innerHTML = d.getMinutes(); //Returns minutes
-//     // document.getElementById('seconds').innerHTML = d.getMinutes(); //Returns seconds
-//     console.log('hi')
-    
-    
-// }
+window.onload = function() {
+    const deg = 6;
+    const hr = document.querySelector("#hr");
+    const mn = document.querySelector("#mn");
+    const sc = document.querySelector("#sc");
+    setInterval(() => {
+        let day = new Date();
+        let hh = day.getHours() * 30;
+        let mm = day.getMinutes() * deg;
+        let ss = day.getSeconds() * deg;
+        hr.style.transform = `rotateZ(${hh + (mm / 12)}deg)`;
+        mn.style.transform = `rotateZ(${mm}deg)`;
+        sc.style.transform = `rotateZ(${ss}deg)`;
+
+        if (7 <= new Date().getHours() && new Date().getHours() >= 20) {
+            var bodyColor = document.getElementsByTagName("body")[0];
+            bodyColor.style.backgroundColor = ' #373B44';
+
+            var clockColor = document.getElementsByClassName('clock')[0];
+            clockColor.style.backgroundColor = '#151515';
+            clockColor.style.border = ' 20px solid white';
+
+            var dateText = document.getElementsByClassName('month')[0];
+            dateText.style.color = '#d6d6d6';
+        }
+        var monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+        var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        document.getElementById('dayName').innerHTML = dayNames[day.getDay()];
+        document.getElementById('date').innerHTML = monthNames[day.getMonth()] + ' ' + day.getDate() + ' ' + day.getFullYear();
+    })
+}
